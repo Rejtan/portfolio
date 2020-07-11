226d1699dc00a9c1b79c90fde43b9ff7f59c047a
Kacper Łagosz <kacperlagosz.exe@gmail.com>
2020-07-11 23:13:07 +0200

    1
    1

---
+++ wp-content/uploads/wp-static-html-output-1594501566/index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<title>Portfolio &#8211; Just another WordPress site</title>
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="Portfolio &raquo; Feed" href="http://rejtan.github.io/portfolio/index.php/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Portfolio &raquo; Comments Feed" href="http://rejtan.github.io/portfolio/index.php/comments/feed/" />
+<link rel='stylesheet' id='wp-block-library-css'  href='http://rejtan.github.io/portfolio/wp-includes/css/dist/block-library/style.min.css?ver=5.4.2' media='all' />
+<link rel='stylesheet' id='wp-block-library-theme-css'  href='http://rejtan.github.io/portfolio/wp-includes/css/dist/block-library/theme.min.css?ver=5.4.2' media='all' />
+<link rel='stylesheet' id='twentynineteen-style-css'  href='http://rejtan.github.io/portfolio/wp-content/themes/twentynineteen/style.css?ver=1.5' media='all' />
+<link rel='stylesheet' id='twentynineteen-print-style-css'  href='http://rejtan.github.io/portfolio/wp-content/themes/twentynineteen/print.css?ver=1.5' media='print' />
+<link rel='https://api.w.org/' href='http://rejtan.github.io/portfolio/index.php/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://rejtan.github.io/portfolio/xmlrpc.php?rsd" />
+<meta name="generator" content="WordPress 5.4.2" />
+<link rel="canonical" href="http://rejtan.github.io/portfolio/" />
+<link rel='shortlink' href='http://rejtan.github.io/portfolio/' />
+<link rel="alternate" type="application/json+oembed" href="http://rejtan.github.io/portfolio/index.php/wp-json/oembed/1.0/embed?url=http%3A%2F%2Frejtan.github.io%2Fportfolio%2F" />
+<link rel="alternate" type="text/xml+oembed" href="http://rejtan.github.io/portfolio/index.php/wp-json/oembed/1.0/embed?url=http%3A%2F%2Frejtan.github.io%2Fportfolio%2F&#038;format=xml" />
+</head>
+
+<body class="home page-template-default page page-id-12 wp-embed-responsive singular image-filters-enabled">
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+
+		<header id="masthead" class="site-header featured-image">
+
+			<div class="site-branding-container">
+				<div class="site-branding">
+
+								<p class="site-title"><a href="http://rejtan.github.io/portfolio/" rel="home">Portfolio</a></p>
+			
+				<p class="site-description">
+				Just another WordPress site			</p>
+			</div><!-- .site-branding -->
+			</div><!-- .site-branding-container -->
+
+							<div class="site-featured-image">
+					
+			<figure class="post-thumbnail">
+				<img width="1200" height="769" src="http://rejtan.github.io/portfolio/wp-content/uploads/2020/07/2020-landscape-1-1.png" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" srcset="http://rejtan.github.io/portfolio/wp-content/uploads/2020/07/2020-landscape-1-1.png 1200w, http://rejtan.github.io/portfolio/wp-content/uploads/2020/07/2020-landscape-1-1-300x192.png 300w, http://rejtan.github.io/portfolio/wp-content/uploads/2020/07/2020-landscape-1-1-1024x656.png 1024w, http://rejtan.github.io/portfolio/wp-content/uploads/2020/07/2020-landscape-1-1-768x492.png 768w" sizes="(max-width: 1200px) 100vw, 1200px" />			</figure><!-- .post-thumbnail -->
+
+								<div class="entry-header">
+						
+<h1 class="entry-title">The New UMoMA Opens its Doors</h1>
+					</div><!-- .entry-header -->
+									</div>
+					</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			
+<article id="post-12" class="post-12 page type-page status-publish has-post-thumbnail hentry entry">
+	
+	<div class="entry-content">
+		<div class="wp-block-group alignwide"><div class="wp-block-group__inner-container"><h2 class="has-text-align-center">The premier destination for modern art in Northern Sweden. Open from 10 AM to 6 PM every day during the summer months.</h2></div></div><div class="wp-block-columns alignwide"><div class="wp-block-column"><div class="wp-block-group"><div class="wp-block-group__inner-container"><figure class="wp-block-image alignfull size-full"><img src="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-three-quarters-1.png" alt="" class="wp-image-37"/></figure><h3>Works and Days</h3><p>August 1 &#8212; December 1</p><div class="wp-block-button is-style-outline"><a class="wp-block-button__link" href="https://make.wordpress.org/core/2019/09/27/block-editor-theme-related-updates-in-wordpress-5-3/">Read More</a></div></div></div><div class="wp-block-group"><div class="wp-block-group__inner-container"><figure class="wp-block-image alignfull size-full"><img src="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-three-quarters-3.png" alt="" class="wp-image-37"/></figure><h3>Theatre of Operations</h3><p>October 1 &#8212; December 1</p><div class="wp-block-button is-style-outline"><a class="wp-block-button__link" href="https://make.wordpress.org/core/2019/09/27/block-editor-theme-related-updates-in-wordpress-5-3/">Read More</a></div></div></div></div><div class="wp-block-column"><div class="wp-block-group"><div class="wp-block-group__inner-container"><figure class="wp-block-image alignfull size-full"><img src="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-three-quarters-2.png" alt="" class="wp-image-37"/></figure><h3>The Life I Deserve</h3><p>August 1 &#8212; December 1</p><div class="wp-block-button is-style-outline"><a class="wp-block-button__link" href="https://make.wordpress.org/core/2019/09/27/block-editor-theme-related-updates-in-wordpress-5-3/">Read More</a></div></div></div><div class="wp-block-group"><div class="wp-block-group__inner-container"><figure class="wp-block-image alignfull size-full"><img src="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-three-quarters-4.png" alt="" class="wp-image-37"/></figure><h3>From Signac to Matisse</h3><p>October 1 &#8212; December 1</p><div class="wp-block-button is-style-outline"><a class="wp-block-button__link" href="https://make.wordpress.org/core/2019/09/27/block-editor-theme-related-updates-in-wordpress-5-3/">Read More</a></div></div></div></div></div><figure class="wp-block-image alignfull size-full"><img src="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-landscape-2.png" alt="" class="wp-image-37"/></figure><div class="wp-block-group alignwide"><div class="wp-block-group__inner-container"><h2 class="has-accent-color has-text-align-center">&#8220;Cyborgs, as the philosopher Donna Haraway established, are not reverent. They do not remember the cosmos.&#8221;</h2></div></div><p class="has-drop-cap">With seven floors of striking architecture, UMoMA shows exhibitions of international contemporary art, sometimes along with art historical retrospectives. Existential, political and philosophical issues are intrinsic to our programme. As visitor you are invited to guided tours artist talks, lectures, film screenings and other events with free admission</p><p>The exhibitions are produced by UMoMA in collaboration with artists and museums around the world and they often attract international attention. UMoMA has received a Special Commendation from the European Museum of the Year, and was among the top candidates for the Swedish Museum of the Year Award as well as for the Council of Europe Museum Prize.</p><p></p><div class="wp-block-group alignwide has-background" style="background-color:#ffffff"><div class="wp-block-group__inner-container"><div class="wp-block-group"><div class="wp-block-group__inner-container"><h2 class="has-text-align-center">Become a Member and Get Exclusive Offers!</h2><p class="has-text-align-center">Members get access to exclusive exhibits and sales. Our memberships cost $99.99 and are billed annually.</p><div class="wp-block-button aligncenter"><a class="wp-block-button__link" href="https://make.wordpress.org/core/2019/09/27/block-editor-theme-related-updates-in-wordpress-5-3/">Join the Club</a></div></div></div></div></div><figure class="wp-block-gallery alignwide columns-2 is-cropped"><ul class="blocks-gallery-grid"><li class="blocks-gallery-item"><figure><img src="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-square-2.png" alt="" data-id="39" data-full-url="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-square-2.png" data-link="assets/images/2020-square-2/" class="wp-image-39"/></figure></li><li class="blocks-gallery-item"><figure><img src="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-square-1.png" alt="" data-id="38" data-full-url="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-square-1.png" data-link="http://rejtan.github.io/portfolio/wp-content/themes/twentytwenty/assets/images/2020-square-1/" class="wp-image-38"/></figure></li></ul></figure>	</div><!-- .entry-content -->
+
+	</article><!-- #post-12 -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		
+	<aside class="widget-area" role="complementary" aria-label="Footer">
+							<div class="widget-column footer-widget-1">
+					<section id="text-4" class="widget widget_text"><h2 class="widget-title">About This Site</h2>			<div class="textwidget"><p>This may be a good place to introduce yourself and your site or include some credits.</p>
+</div>
+		</section>					</div>
+					</aside><!-- .widget-area -->
+
+		<div class="site-info">
+										<a class="site-name" href="http://rejtan.github.io/portfolio/" rel="home">Portfolio</a>,
+						<a href="https://wordpress.org/" class="imprint">
+				Proudly powered by WordPress.			</a>
+								</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+	
+</body>
+</html>
